7770764f865e6797b4e86c8ac1a42e436e47e4a6
Rafael Santos <rafael.santos@4effect.com.br>
2019-09-19 21:51:05 -0300

    product list created

--- src/app/components/newsletter/header/header.component.ts
+++ src/app/components/newsletter/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  geToKnowLinx() {
+    // Get to know action here
+  }
+
+  helpTheCode() {
+    // Help the code action here
+  }
+
+  goToProducts() {
+    // Go to products action here
+  }
+
+  share() {
+    // Share action here
+  }
 }
--- src/app/components/newsletter/products-list/products-list.component.ts
+++ src/app/components/newsletter/products-list/products-list.component.ts
@@ -1,5 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Product} from '../../../models/product.model';
+import {ProductService} from '../../../services/product.service';
+import {take} from 'rxjs/operators';
+import {ApiResponse} from '../../../models/apiResponse.model';
+import {HttpResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-products-list',
@@ -9,27 +14,54 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 export class ProductsListComponent implements OnInit {
 
   form: FormGroup;
+  products: Product[];
+  page: number;
 
   constructor(
-    private formBuider: FormBuilder
+    private formBuider: FormBuilder,
+    private productService: ProductService
   ) {
+    this.page = 1;
+    this.products = [];
   }
 
   ngOnInit() {
     this.createForm();
+    this.listProducts();
   }
 
-  share() {
+  private listProducts(): void {
+    this.productService.listProducts(this.page)
+      .pipe(take(1))
+      .subscribe((apiResponse: HttpResponse<ApiResponse>) => {
+        this.products.push(...apiResponse.body.products);
+      });
+  }
+
+  share(): void {
     if (this.form.invalid) {
       return;
     }
     // Share function here
   }
 
-  private createForm() {
+  private createForm(): void {
     this.form = this.formBuider.group({
       friendsName: [null, [Validators.required]],
       email: [null, [Validators.required, Validators.email]]
     });
+  }
+
+  formatPrice(price: number): string {
+    return price.toFixed(2).replace('.', ',');
+  }
+
+  getMoreProducts() {
+    this.page ++;
+    this.listProducts();
+  }
+
+  buyProduct(product: Product) {
+    // Buy function here
   }
 }
