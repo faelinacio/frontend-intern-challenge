2352641b243210f2de6930dacd250ebe8d170850
Rafael Santos <rafael.santos@4effect.com.br>
2019-09-21 10:46:37 -0300

    input validation testes created

--- src/app/components/newsletter/products-list/products-list.component.spec.ts
+++ src/app/components/newsletter/products-list/products-list.component.spec.ts
@@ -31,4 +31,39 @@ describe('ProductsListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Friend name input should be required', (() => {
+    const nameInput = component.form.controls.friendsName;
+    nameInput.setValue('');
+    expect(nameInput.valid).toBeFalsy();
+    let errors = nameInput.errors || {};
+    expect(errors.required).toBeTruthy();
+    nameInput.setValue('Name');
+    expect(nameInput.valid).toBeTruthy();
+    errors = nameInput.errors || {};
+    expect(errors.required).toBeFalsy();
+  }));
+
+  it('E-mail input should be required', (() => {
+    const emailInput = component.form.controls.email;
+    emailInput.setValue('');
+    expect(emailInput.valid).toBeFalsy();
+    let errors = emailInput.errors || {};
+    expect(errors.required).toBeTruthy();
+    emailInput.setValue('Email');
+    errors = emailInput.errors || {};
+    expect(errors.required).toBeFalsy();
+  }));
+
+  it('E-mail input should be a valid e-mail', (() => {
+    const emailInput = component.form.controls.email;
+    emailInput.setValue('Email');
+    expect(emailInput.valid).toBeFalsy();
+    let errors = emailInput.errors || {};
+    expect(errors.email).toBeTruthy();
+    emailInput.setValue('Email@email.com');
+    expect(emailInput.valid).toBeTruthy();
+    errors = emailInput.errors || {};
+    expect(errors.email).toBeFalsy();
+  }));
 });
